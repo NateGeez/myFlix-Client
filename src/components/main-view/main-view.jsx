0989b8e2234dc8a100ec4b9388c46df820413db5
NateGeez <160933873+NateGeez@users.noreply.github.com>
2024-07-26 15:48:47 -0500

    added login-view and sign-up view with authentication

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,42 +1,40 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
-    const [error, setError] = useState(null);
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
 
     useEffect(() => {
-        fetch("https://natesmovieflix-742bdbb68d51.herokuapp.com/movies")
-            .then((response) => {
-                if (response.headers.get("content-type")?.includes("application/json")) {
-                    return response.json();
-                } else {
-                    throw new Error("Response was not JSON");
-                }
-            })
+        if (!token) {
+            return;
+        }
+
+        fetch("https://natesmovieflix-742bdbb68d51.herokuapp.com/movies", {
+            headers: { Authorization: 'Bearer ${token}' }
+        })
+            .then((response) => response.json())
             .then((data) => {
-                if (!data || !Array.isArray(data)) {
-                    throw new Error('Invalid data format');
-                }
-
-                const moviesFromApi = data.map((doc) => ({
-                    id: doc._id,
-                    title: doc.Title,
-                    director: doc.Director?.Name,
-                }));
-
-                setMovies(moviesFromApi);
-            })
-            .catch((err) => {
-                console.error('Fetch error:', err);
-                setError(err.message);
+                console.log(data);
             });
-    }, []);
+    }, [token]);
 
-    if (error) {
-        return <div>Error: {error}</div>;
+    if (!user) {
+        return (
+            <>
+            <LoginView onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                 }} />
+                 or
+                 <SignupView />
+            </>
+        );
     }
 
     if (selectedMovie) {
@@ -60,6 +58,7 @@ export const MainView = () => {
                     }}
                 />
             ))}
+            <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
         </div>
     );
 };
