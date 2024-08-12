09ae43d45630673975cd35d8dff704e4d1698f90
NateGeez <160933873+NateGeez@users.noreply.github.com>
2024-08-12 07:51:03 -0500

    added react router

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,18 @@
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={movie.ImagePath} alt={movie.Title} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text><strong>Director:</strong> {movie.Director.Name}</Card.Text>
-                <Button onClick={() => onMovieClick(movie)} variant="info">
-                    Open
-                </Button>
-            </Card.Body>            
+                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                    <Button variant="info">Open</Button>
+                </Link>
+            </Card.Body>
         </Card>
     );
 };
@@ -23,6 +24,5 @@ MovieCard.propTypes = {
         Director: PropTypes.shape({
             Name: PropTypes.string.isRequired
         }).isRequired
-    }).isRequired, 
-    onMovieClick: PropTypes.func.isRequired
+    }).isRequired
 };
